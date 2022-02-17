5db39ff8f497639ea90f2d9c060d49f497cb1770
Pramendra Gupta <pramendra@gmail.com>
2022-02-17 22:12:00 +0900

    added page size

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,6 +2,7 @@ class Constants {
   static String baseURLHN = "https://hacker-news.firebaseio.com/v0";
 }
 
-final String topStoryType = 'top';
-final String newStoryType = 'new';
-final String bestStoryType = 'best';
+const String topStoryType = 'top';
+const String newStoryType = 'new';
+const String bestStoryType = 'best';
+const int pageSize = 20;
