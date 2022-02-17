a6c920d4c85dce7f692de803c734a2d189ae4859
Pramendra Gupta <pramendra@gmail.com>
2022-02-17 22:13:00 +0900

    added story type

--- lib/views/stories_list.dart
+++ lib/views/stories_list.dart
@@ -3,16 +3,22 @@ import 'package:hnnews/models/story_model.dart';
 import 'package:hnnews/views/story_tile.dart';
 
 class StoriesList extends StatelessWidget {
-  StoriesList({Key? key, required this.stories, required this.isLoading}) {}
+  StoriesList({
+    Key? key,
+    required this.stories,
+    required this.isLoading,
+    required this.storyType,
+  }) {}
 
   List<StoryModel> stories;
   bool isLoading;
+  String storyType;
 
   @override
   Widget build(BuildContext context) {
     // isLoading
     return ListView.builder(
-      key: PageStorageKey<String>(key.toString()),
+      key: PageStorageKey<String>(storyType),
       itemCount: stories.length,
       itemBuilder: (context, index) {
         return StoryTile(story: stories[index]);
