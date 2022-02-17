a7d39bf2370dba0abbebef804980d0392c13d998
Pramendra Gupta <pramendra@gmail.com>
2022-02-17 22:21:00 +0900

    refactored services

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -3,25 +3,52 @@ import 'package:http/http.dart' as http;
 import 'package:hnnews/services/url_services.dart';
 import 'package:hnnews/models/story_model.dart';
 
-Future<List<StoryModel>> fetchTopStories() async {
-  var result = <StoryModel>[];
+Future<List<int>> fetchStoryIds(String url) async {
   final res = await http.get(
-    Uri.parse(URLService.topStories()),
+    Uri.parse(url),
   );
   if (res.statusCode == 200) {
     var data = StoriesModel.fromJson(jsonDecode(res.body));
+    return data.stories;
+  } else {
+    throw Exception('fetchStoryIds failed');
+  }
+}
 
-    data.stories.map((id) async {
-      var storyData = await fetchStoryByID(id);
-      print(storyData);
-      result.add(storyData);
-    });
-
-    return result;
+Future<List<StoryModel>> fetchOneStories(Iterable stories) async {
+  if (stories.isNotEmpty) {
+    return Future.wait(stories.map((storyId) {
+      return fetchStoryByID(storyId);
+    }));
   } else {
     throw Exception('top stories fetch failed');
   }
 }
+
+// Future<List<StoryModel>> fetchTopStories() async {
+//   // var result = <StoryModel>[];
+//   final res = await http.get(
+//     Uri.parse(URLService.topStories()),
+//   );
+//   if (res.statusCode == 200) {
+//     var data = StoriesModel.fromJson(jsonDecode(res.body));
+//     // print(data);
+//     // data.stories.map((id) async {
+//     //   var storyData = await fetchStoryByID(id);
+//     //   print(storyData);
+//     //   result.add(storyData);
+//     // });
+//     // Iterable storyIds = jsonDecode(res.body);
+//     print(data.stories.length);
+//     return Future.wait(data.stories.take(40).map((storyId) {
+//       return fetchStoryByID(storyId);
+//     }));
+
+//     // return result;
+//   } else {
+//     throw Exception('top stories fetch failed');
+//   }
+// }
 
 Future<StoryModel> fetchStoryByID(int id) async {
   final res = await http.get(
