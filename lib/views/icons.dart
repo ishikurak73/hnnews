64d50918bb8b5a8609320f08c8b817d17ba562b5
Pramendra Gupta <pramendra@gmail.com>
2022-02-20 20:37:39 +0900

    refactored icon

--- lib/views/icons.dart
+++ lib/views/icons.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
+const double smallSize = 18;
+const double largeSize = 24;
+
 const likeIcon = Icon(
   Icons.favorite,
-  // color: Colors.black,
-  size: 25,
+  size: largeSize,
   key: Key('like_icon'),
 );
 
 const likedIcon = Icon(
   Icons.favorite_outline_sharp,
-  // color: Colors.red,
-  size: 25,
+  size: largeSize,
   key: Key('liked_icon'),
+);
+
+const smallLikeIcon = Icon(
+  Icons.favorite,
+  size: smallSize,
+  key: Key('like_icon'),
+);
+
+const smallLikedIcon = Icon(
+  Icons.favorite_outline_sharp,
+  size: smallSize,
+  key: Key('liked_icon'),
+);
+
+const smallCommentIcon = Icon(
+  Icons.comment_outlined,
+  size: smallSize,
+  key: Key('comment_icon'),
 );
